3204f9f7072a79981d717e67687eef4bdf960681
Nagpal45 <nagpal4503@gmail.com>
2024-04-18 17:55:00 +0530

    mongoose model

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express  = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Db connected');
+})
+
 const app = express();
 
 app.listen(8000, ()=>{
